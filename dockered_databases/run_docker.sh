3c73ac124a06afe2f2fc29fba9dc16e54b64eadc
Nikita Tomilov <programmer174@icloud.com>
2020-04-01 19:26:13 +0300

    dockered_databases: add/verify persistent database data for all containers

--- dockered_databases/run_docker.sh
+++ dockered_databases/run_docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 mkdir -p ~/st_and_db_labs_data/oracle
+mkdir -p ~/st_and_db_labs_data/mongo
+mkdir -p ~/st_and_db_labs_data/mysql
+mkdir -p ~/st_and_db_labs_data/postgre
 docker-compose build
 docker-compose up
