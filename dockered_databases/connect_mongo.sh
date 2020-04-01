3c373f9d085273a5e2de9c6b0ac482d2ded65613
Nikita Tomilov <programmer174@icloud.com>
2020-04-01 18:10:32 +0300

    dockered_databases: dockerify MongoDB

---
+++ dockered_databases/connect_mongo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mongo --username admin --password admin --authenticationDatabase admin --host localhost --port 27017 st_and_db_mongo_database
---
+++ dockered_databases/containers/mongo/init.js
@@ -0,0 +1,12 @@
+db.createUser(
+        {
+            user: "admin",
+            pwd: "admin",
+            roles: [
+                {
+                    role: "readWrite",
+                    db: "st_and_db_mongo_database"
+                }
+            ]
+        }
+);
